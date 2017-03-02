52eb1996a3a56dfcc2e520a324b46751a4079644
kinoshitakeisuke <k.kinoshita1214@gmail.com>
2017-03-02 18:51:35 +0900

    2017/3/2

---
+++ src/bulletinBoard/controller/StopServlet.java
@@ -0,0 +1,29 @@
+package bulletinBoard.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bulletinBoard.beans.User;
+import bulletinBoard.service.UserService;
+
+
+@WebServlet(urlPatterns = { "/stop" })
+public class StopServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		User user = new User();
+		user.setId(Integer.parseInt(request.getParameter("management.id")));
+		user.setIs_stoped(Integer.parseInt(request.getParameter("is_stoped")));
+		new UserService().invert(user);
+
+		response.sendRedirect("./");
+	}
+
+}
